3c7dbc7a98486203bf81ce8473d37858c2112b32
flatmush <flatmush@52aa303c-2c39-11df-b580-1f2e1e9a714e>
2010-03-31 21:38:58 +0000

    Fixed a few glitches in the encoding of sml sprites, should now work with all sprites. Added dma_free function to dma.c which gives an integer representing the first free dma channel.

--- trunk/dingoo_sdk/src/sml/dma.h
+++ trunk/dingoo_sdk/src/sml/dma.h
@@ -4,8 +4,9 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-extern bool dma_wait(uintptr_t inChannel, uintptr_t inTimeout);
-extern bool dma_copy(uintptr_t inChannel, void* inDest, void* inSource, uintptr_t inSize);
-extern bool dma_set(uintptr_t inChannel, void* inDest, uintptr_t inDestSize, void* inSource, uintptr_t inSourceSize);
+extern uintptr_t dma_free();
+extern bool      dma_wait(uintptr_t inChannel, uintptr_t inTimeout);
+extern bool      dma_copy(uintptr_t inChannel, void* inDest, void* inSource, uintptr_t inSize);
+extern bool      dma_set(uintptr_t inChannel, void* inDest, uintptr_t inDestSize, void* inSource, uintptr_t inSourceSize);
 
 #endif
