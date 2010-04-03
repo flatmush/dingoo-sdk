f6b9437c9dd35d7d8b0621061d138444f5649f23
russell.bernhardt@gmail.com <russell.bernhardt@gmail.com@52aa303c-2c39-11df-b580-1f2e1e9a714e>
2010-04-03 23:59:38 +0000

    Commented two header files, removed old doc/wiki folder since I don't use it for wiki pages.

--- dingoo_sdk/src/sml/display.h
+++ dingoo_sdk/src/sml/display.h
@@ -1,9 +1,29 @@
 #ifndef __display_h__
 #define __display_h__
-
+/**
+=sml/display.h=
+Controls the LCD display.
+*/
 #include <stdbool.h>
 #include <stdint.h>
 
+/**
+==Macros==
+===Display format flags===
+  * *DISPLAY`_`FORMAT`_`NONE*: Reset all the display format flags
+  * *DISPLAY`_`FORMAT`_`GRAY4*: Use 4-color grayscale
+  * *DISPLAY`_`FORMAT`_`GRAY8*: Use 8-color grayscale
+  * *DISPLAY`_`FORMAT`_`CLUT4*: Use a 4-color palette
+  * *DISPLAY`_`FORMAT`_`CLUT8*: Use an 8-color palette
+  * *DISPLAY`_`FORMAT`_`RGB332*: Use a 332-color palette
+  * *DISPLAY`_`FORMAT`_`RGB565*: Use a 565-color palette
+  * *DISPLAY`_`FORMAT`_`RGB888*: Use an 888-color palette
+  * *DISPLAY`_`FORMAT`_`RGBA2222*: Use a 2,222-color palette with transparency
+  * *DISPLAY`_`FORMAT`_`RGBA5551*: Use a 5,551-color palette with transparency
+  * *DISPLAY`_`FORMAT`_`RGBA4444*: Use a 4,444-color palette with transparency
+  * *DISPLAY`_`FORMAT`_`RGBA8888*: Use an 8,888-color palette with transparency
+  * *DISPLAY`_`FORMAT`_`LAST*: Use the highest format (8,888-color palette with transparency)
+*/
 #define DISPLAY_FORMAT_NONE     0
 #define DISPLAY_FORMAT_GRAY4    1
 #define DISPLAY_FORMAT_GRAY8    2
@@ -18,6 +38,17 @@
 #define DISPLAY_FORMAT_RGBA8888 11
 #define DISPLAY_FORMAT_LAST     DISPLAY_FORMAT_RGBA8888
 
+/**
+===Display mode flags===
+  * *DISPLAY`_`HFLIP*: Flip the display horizontally
+  * *DISPLAY`_`VFLIP*: Flip the display vertically
+  * *DISPLAY`_`ROTATE*: Rotate the display
+  * *DISPLAY`_`STRETCH*: Stretch the display
+  * *DISPLAY`_`FILTER`_`NEAREST*: Stretch using a nearest-neighbor resize algorithm (usually ugly but very fast)
+  * *DISPLAY`_`FILTER`_`LINEAR*: Stretch using a linear resize algorithm (usually nicer than nearest-neighbor, but slower)
+  * *DISPLAY`_`BUFFER`_`STATIC*: TODO
+  * *DISPLAY`_`PALETTE`_`STATIC*: TODO
+*/
 #define DISPLAY_HFLIP          (1 << 4)
 #define DISPLAY_VFLIP          (1 << 5)
 #define DISPLAY_ROTATE         (1 << 6)
@@ -27,8 +58,17 @@
 #define DISPLAY_BUFFER_STATIC  (1 << 9)
 #define DISPLAY_PALETTE_STATIC (1 << 10)
 
-
-
+/**
+==Structs==
+===display===
+Members:
+  * _uint16_t_ *width*: Stores the display's width in pixels
+  * _uint16_t_ *height*: Stores the display's height in pixels
+  * _uint16_t_ *stride*: TODO
+  * _uint32_t_ *flags*: Stores all the set flags for this display
+  * _void_`*` *buffer*: Stores a pointer to the buffer in memory
+  * _void_`*` *palette*: Stores a pointer to the palette in memory
+*/
 typedef struct {
 	uint16_t width, height, stride;
 	uint32_t flags;
@@ -36,14 +76,18 @@ typedef struct {
 	void*    palette;
 } display;
 
-
-
+/**
+==Functions==
+  * [#display display]`*` *display`_`create*( _uint16_t_ width, _uint16_t_ height, _uint16_t_ stride, _uint32_t_ flags, _void_`*` buffer, _void_`*` palette ): Returns a pointer to the created display with the specified attributes.
+  * [#display display]`*` *display`_`create_fastest*( _void_ ): Returns a pointer to a display with Dingoo defaults in mind (320x240 screen, full stride, 565-color static buffer) with no buffer or palette specified.
+  * _void_ *display`_`delete*( [#display display]`*` ): Frees the specified display from memory.
+  * _void_ *display`_`clear*( [#display display]`*` ): Clears the contents of the specified display.
+  * _void_ *display`_`flip*( [#display display]`*` ): Pushes the specified display into view on the LCD.
+**/
 extern display* display_create(uint16_t inWidth, uint16_t inHeight, uint16_t inStride, uint32_t inFlags, void* inBuffer, void* inPalette);
 extern display* display_create_fastest();
 extern void     display_delete(display* inDisplay);
-
 extern void     display_clear(display* inDisplay);
-
 extern void*    display_flip(display* inDisplay);
 
 // TODO - Support non-native palette formats.
--- dingoo_sdk/src/sml/fixmath.h
+++ dingoo_sdk/src/sml/fixmath.h
@@ -1,17 +1,59 @@
 #ifndef __fixmath_h__
 #define __fixmath_h__
-
+/**
+=sml/fixmath.h=
+Functions to perform fast fixed-point math operations.
+*/
 #include <stdint.h>
 #include <stdbool.h>
 
-extern uint32_t uint32_log2(uint32_t inVal);
-
-typedef int32_t fix16_t;
 #define fix16_MAX (fix16_t)0x7FFFFFFF
 #define fix16_MIN (fix16_t)0x80000000
+/**
+==Types==
+  * *fract32`_`t*: Just a _uint32`_`t_.
+  * *fix16`_`t*: Just an _int32`_`t_.
+*/
+typedef uint32_t fract32_t;
+typedef int32_t fix16_t;
+
+/**
+==Global constants==
+  * *fix16`_`pi*: Pi (3.14... TODO)
+  * *fix16`_`e*: e (... TODO)
+  * *fix16`_`one*: 1.0000000.. TODO
+*/
 extern const fix16_t fix16_pi;
 extern const fix16_t fix16_e;
 extern const fix16_t fix16_one;
+
+/**
+==Functions==
+  * _uint32`_`t_ *uint32`_`log2*( _uint32`_`t_ ): Performs a log-base2 on the given number.
+  * _double_ *fix16`_`to`_`dbl*( [#Types fix16_t] ): Converts the given fixed-point to a double.
+  * [#Types fix16_t] *fix16`_`from`_`dbl*( _double_ ): Converts the given double to a fixed-point.
+  * _int32`_`t_ *fix16`_`to`_`int*( [#Types fix16_t] ): Converts the given fixed-point to an integer.
+  * [#Types fix16_t] *fix16`_`from`_`int*( _int32`_`t_ ): Converts the given integer to a fixed-point.
+  * [#Types fix16_t] *fix16`_`sadd*( [#Types fix16_t], [#Types fix16_t] ): Adds the two fixed-points together.
+  * [#Types fix16_t] *fix16`_`mul*( [#Types fix16_t], [#Types fix16_t] ): Multiplies the two fixed-points together.
+  * [#Types fix16_t] *fix16`_`smul*( [#Types fix16_t], [#Types fix16_t] ): Multiplies the two _signed_ fixed-points together.
+  * [#Types fix16_t] *fix16`_`div*( [#Types fix16_t], [#Types fix16_t] ): Divides the first fixed-point by the second fixed-point.
+  * [#Types fix16_t] *fix16`_`sdiv*( [#Types fix16_t], [#Types fix16_t] ): Divides the first _signed_ fixed-point by the second _signed_ fixed-point.
+  * [#Types fix16_t] *fix16`_`sin*( [#Types fix16_t] ): Performs a sine on the given fixed-point.
+  * [#Types fix16_t] *fix16`_`cos*( [#Types fix16_t] ): Performs a cosine on the given fixed-point.
+  * [#Types fix16_t] *fix16`_`tan*( [#Types fix16_t] ): Performs a tangent on the given fixed-point.
+  * [#Types fix16_t] *fix16`_`asin*( [#Types fix16_t] ): Performs an inverse sine on the given fixed-point.
+  * [#Types fix16_t] *fix16`_`scos*( [#Types fix16_t] ): Performs an inverse cosine on the given fixed-point.
+  * [#Types fix16_t] *fix16`_`atan*( [#Types fix16_t] ): Performs an inverse tangent on the given fixed-point.
+  * [#Types fix16_t] *fix16`_`atan2*( [#Types fix16_t] ): Performs an inverse base2-tangent on the given fixed-point. TODO
+  * [#Types fix16_t] *fix16`_`sqrt*( [#Types fix16_t] ): Performs a square root on the given fixed-point.
+  * [#Types fix16_t] *fix16`_`lerp16*( [#Types fix16_t] ): TODO
+  * [#Types fract32_t] *fract32`_`create*( _uint32`_`t_, _uint32`_`t_ ): Creates a fraction with the first integer as the numerator, and the second as the denominator.
+  * [#Types fract32_t] *fract32`_`invert*( [#Types fract32_t] ): Inverts the given fraction.
+  * _uint32`_`t_  *fract32`_`usmul*( _uint32`_`t_, [#Types fract32_t] ): Multiplies the given fraction by the given unsigned integer.
+  * _uint32`_`t_  *fract32`_`smul*( _int32`_`t_, [#Types fract32_t] ): Multiplies the given fraction by the given signed integer. 
+*/
+extern uint32_t uint32_log2(uint32_t inVal);
 
 extern double  fix16_to_dbl(const fix16_t inVal);
 extern fix16_t fix16_from_dbl(const double inVal);
@@ -40,8 +82,6 @@ extern fix16_t fix16_atan2(fix16_t inY, fix16_t inX);
 extern fix16_t fix16_sqrt(fix16_t inValue);
 
 extern fix16_t fix16_lerp16(fix16_t inArg0, fix16_t inArg1, uint16_t inFract);
-
-typedef uint32_t fract32_t;
 
 extern fract32_t fract32_create(uint32_t inNumerator, uint32_t inDenominator);
 extern fract32_t fract32_invert(fract32_t inFract);
