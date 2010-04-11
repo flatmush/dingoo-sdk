be8c4d8589cf7038cfd2675b0540e28be0a2c9e3
flatmush <flatmush@52aa303c-2c39-11df-b580-1f2e1e9a714e>
2010-04-11 22:36:17 +0000

    Fixed issue 1 finally, threaded mixing works so sound should run smoother and better in most circumstances.

--- dingoo_sdk/src/sml/sound.c
+++ dingoo_sdk/src/sml/sound.c
@@ -18,13 +18,26 @@ typedef struct {
 } _channel_t;
 
 
+
 volatile uintptr_t  _sound_sources_count = 16;
 volatile _channel_t _sound_sources[16];
 OS_EVENT*  _sound_sources_semaphore = NULL;
 
-uintptr_t _sound_buffer_size = 4096;
-uintptr_t _sound_buffer_used = 0;
-int16_t   _sound_buffer[4096];
+int16_t   _sound_buffer[3][4096];
+uintptr_t _sound_buffer_used[3]  = { 0, 0, 0 };
+uintptr_t _sound_buffer_size     = 4096;
+uintptr_t _sound_buffer_volume   = 100;
+
+uintptr_t _sound_buffer_mixing   = 0;
+uintptr_t _sound_buffer_playnext = 3;
+uintptr_t _sound_buffer_playing  = 1;
+
+uint8_t       _sound_mixer_thread_id         = 255;
+uintptr_t     _sound_mixer_thread_stack_size = 0;
+OS_STK*       _sound_mixer_thread_stack      = NULL;
+volatile bool _sound_mixer_thread_kill       = false;
+
+bool _sound_ready = false;
 
 uintptr_t _sound_id_max = 1;
 
@@ -38,7 +51,9 @@ uintptr_t _sound_id_max = 1;
 #define min(x, y) ((x) < (y) ? (x) : (y))
 #endif
 
-
+#ifndef swap
+#define swap(x, y) { typeof(y) __swap = (y); (y) = (x); (x) = __swap; }
+#endif
 
 inline int16_t _ssadd(int16_t inArg0, int16_t inArg1) {
 	int32_t tempOut = (int32_t)inArg0 + (int32_t)inArg1;
@@ -52,16 +67,16 @@ inline void _sound_channel_mix(volatile _channel_t* inChannel) {
 		return;
 
 	uintptr_t i, j, j_max;
-	for(i = 0; i < _sound_buffer_used;) {
+	for(i = 0; i < _sound_buffer_used[_sound_buffer_mixing];) {
 		if(inChannel->stereo) {
-			j_max = inChannel->sample_offset + min((_sound_buffer_used - i), (inChannel->sample_count - inChannel->sample_offset));
+			j_max = inChannel->sample_offset + min((_sound_buffer_used[_sound_buffer_mixing] - i), (inChannel->sample_count - inChannel->sample_offset));
 			for(j = inChannel->sample_offset; j < j_max; j++, i++)
-				_sound_buffer[i] = _ssadd(_sound_buffer[i], inChannel->sample_data[j]);
+				_sound_buffer[_sound_buffer_mixing][i] = _ssadd(_sound_buffer[_sound_buffer_mixing][i], inChannel->sample_data[j]);
 		} else {
-			j_max = inChannel->sample_offset + min(((_sound_buffer_used - i) >> 1), (inChannel->sample_count - inChannel->sample_offset));
+			j_max = inChannel->sample_offset + min(((_sound_buffer_used[_sound_buffer_mixing] - i) >> 1), (inChannel->sample_count - inChannel->sample_offset));
 			for(j = inChannel->sample_offset; j < j_max; j++, i += 2) {
-				_sound_buffer[i + 0] = _ssadd(_sound_buffer[i + 0], inChannel->sample_data[j]);
-				_sound_buffer[i + 1] = _ssadd(_sound_buffer[i + 1], inChannel->sample_data[j]);
+				_sound_buffer[_sound_buffer_mixing][i + 0] = _ssadd(_sound_buffer[_sound_buffer_mixing][i + 0], inChannel->sample_data[j]);
+				_sound_buffer[_sound_buffer_mixing][i + 1] = _ssadd(_sound_buffer[_sound_buffer_mixing][i + 1], inChannel->sample_data[j]);
 			}
 		}
 		inChannel->sample_offset = j;
@@ -78,68 +93,132 @@ inline void _sound_channel_mix(volatile _channel_t* inChannel) {
 		if(inChannel->stereo) {
 			j_max = inChannel->sample_offset + min((_sound_buffer_size - i), (inChannel->sample_count - inChannel->sample_offset));
 			for(j = inChannel->sample_offset; j < j_max; j++, i++)
-				_sound_buffer[i] = inChannel->sample_data[j];
+				_sound_buffer[_sound_buffer_mixing][i] = inChannel->sample_data[j];
 		} else {
 			j_max = inChannel->sample_offset + min(((_sound_buffer_size - i) >> 1), (inChannel->sample_count - inChannel->sample_offset));
 			for(j = inChannel->sample_offset; j < j_max; j++, i += 2) {
-				_sound_buffer[i + 0] = inChannel->sample_data[j];
-				_sound_buffer[i + 1] = inChannel->sample_data[j];
+				_sound_buffer[_sound_buffer_mixing][i + 0] = inChannel->sample_data[j];
+				_sound_buffer[_sound_buffer_mixing][i + 1] = inChannel->sample_data[j];
 			}
 		}
 		inChannel->sample_offset = j;
 		if(inChannel->sample_offset >= inChannel->sample_count) {
 			if(!inChannel->loop) {
 				inChannel->used = false;
-				_sound_buffer_used = i;
+				_sound_buffer_used[_sound_buffer_mixing] = i;
 				return;
 			}
 			inChannel->sample_offset -= inChannel->sample_count;
 		}
 	}
 
-	_sound_buffer_used = i;
+	_sound_buffer_used[_sound_buffer_mixing] = i;
 }
 
 
 
-void _sound_callback() {
+void _sound_mixer_thread(void* inDummy) {
 	uintptr_t i;
-
 	uint8_t tempError;
-	OSSemPend(_sound_sources_semaphore, 0, &tempError);
 
-	_sound_buffer_used = 0;
-	for(i = 0; i < _sound_sources_count; i++)
-		_sound_channel_mix(&_sound_sources[i]);
+	while(!_sound_mixer_thread_kill) {
+		_sound_buffer_mixing = 0;
+		while((_sound_buffer_mixing == _sound_buffer_playnext) || (_sound_buffer_mixing == _sound_buffer_playing))
+			_sound_buffer_mixing++;
 
-	OSSemPost(_sound_sources_semaphore);
+		OSSemPend(_sound_sources_semaphore, 0, &tempError);
+		_sound_buffer_used[_sound_buffer_mixing] = 0;
+		for(i = 0; i < _sound_sources_count; i++)
+			_sound_channel_mix(&_sound_sources[i]);
+		OSSemPost(_sound_sources_semaphore);
 
-	if(_sound_buffer_used == 0) {
+		while(_sound_buffer_playnext < 3)
+			OSTimeDly(1);
+		_sound_buffer_playnext = _sound_buffer_mixing;
+	}
+	_sound_mixer_thread_kill = false;
+	OSTaskDel(_sound_mixer_thread_id);
+}
+
+void _sound_callback() {
+	_sound_buffer_playing = 3;
+
+	// Wait until there is a buffer to be played.
+	while(_sound_buffer_playnext >= 3)
+		OSTimeDly(1);
+
+	_sound_buffer_playing  = _sound_buffer_playnext;
+	_sound_buffer_playnext = 3;
+
+	if(_sound_buffer_used[_sound_buffer_playing] == 0) {
 		mtaudio_buffer_set(NULL, 0, 2, 100);
 		return;
 	}
 
-	mtaudio_buffer_set(_sound_buffer, (_sound_buffer_used << 1), 2, 100);
+	mtaudio_buffer_set(_sound_buffer[_sound_buffer_playing], (_sound_buffer_used[_sound_buffer_playing] << 1), 2, _sound_buffer_volume);
 }
 
 
 
 bool sound_init() {
+	if(_sound_ready)
+		sound_term();
+
 	uintptr_t i;
 	for(i = 0; i < _sound_sources_count; i++)
 		_sound_sources[i].used = false;
+
 	_sound_sources_semaphore = OSSemCreate(1);
 	if(_sound_sources_semaphore == NULL)
 		return false;
+
 	if(!mtaudio_init(_sound_callback)) {
 		OSSemDel(_sound_sources_semaphore, OS_DEL_NO_PEND, NULL);
 		_sound_sources_semaphore = NULL;
 		return false;
 	}
-	return true;
+
+	_sound_mixer_thread_stack_size = 1024;
+	_sound_mixer_thread_stack = (OS_STK*)malloc(_sound_mixer_thread_stack_size * sizeof(OS_STK));
+	if(_sound_mixer_thread_stack == NULL) {
+		mtaudio_term();
+		OSSemDel(_sound_sources_semaphore, OS_DEL_NO_PEND, NULL);
+		_sound_sources_semaphore = NULL;
+		return false;
+	}
+
+	uint8_t j;
+	for(i = 6;; i++) {
+		j = OSTaskCreate(_sound_mixer_thread, NULL, (void*)&_sound_mixer_thread_stack[_sound_mixer_thread_stack_size - 1], i);
+		if(j == OS_NO_ERR)
+			break;
+		if(j >= OS_PRIO_INVALID) {
+			free(_sound_mixer_thread_stack);
+			_sound_mixer_thread_stack = NULL;
+			mtaudio_term();
+			OSSemDel(_sound_sources_semaphore, OS_DEL_NO_PEND, NULL);
+			_sound_sources_semaphore = NULL;
+			return false;
+		}
+	}
+	_sound_mixer_thread_id = i;
+
+	_sound_ready = true;
+	return _sound_ready;
 }
 
 void sound_term() {
+	if(!_sound_ready)
+		return;
+	_sound_ready = false;
+
+	_sound_mixer_thread_kill = true;
+	while(_sound_mixer_thread_kill)
+		OSTimeDly(1);
+
+	free(_sound_mixer_thread_stack);
+	_sound_mixer_thread_stack = NULL;
+
 	mtaudio_term();
 	uint8_t tempError;
 	OSSemDel(_sound_sources_semaphore, OS_DEL_NO_PEND, &tempError);
@@ -192,4 +271,12 @@ void sound_stop(uintptr_t inSound) {
 	}
 
 	OSSemPost(_sound_sources_semaphore);
+}
+
+
+
+uintptr_t sound_volume_set(uintptr_t inVolume) {
+	uintptr_t tempVol = max(inVolume, 100);
+	_sound_buffer_volume = tempVol;
+	return tempVol;
 }
