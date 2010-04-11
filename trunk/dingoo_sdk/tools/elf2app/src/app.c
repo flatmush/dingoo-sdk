54a1d63f1cb401feb39bbca0d1c23ffa4c9024e0
flatmush <flatmush@52aa303c-2c39-11df-b580-1f2e1e9a714e>
2010-04-11 18:04:35 +0000

    Re-wrote app2elf to actually create a correct app file rather than just copying a header and patching it. Now correctly links all external functions for app files and should correctly export for sim too but untested. Now correctly handles .bss segment so that uninitialized data isn't saved in the app file.

---
+++ trunk/dingoo_sdk/tools/elf2app/src/app.c
@@ -0,0 +1,330 @@
+#include "app.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+
+
+typedef struct {
+	char     ident[4];
+	uint8_t  unknown[20];
+	uint8_t  padding[8];
+} __attribute__((__packed__)) _app_ccdl;
+
+_app_ccdl _app_ccdl_default = {
+	{ 'C', 'C', 'D', 'L' },
+	{
+		0x00, 0x00, 0x01, 0x00,
+		0x01, 0x00, 0x02, 0x00,
+		0x04, 0x00, 0x00, 0x00,
+		0x20, 0x09, 0x06, 0x24,
+		0x19, 0x24, 0x42, 0x00
+	},
+	{
+		0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00
+	}
+};
+
+
+
+typedef struct {
+	char     ident[4];
+	uint32_t unknown;
+	uint32_t offset;
+	uint32_t size;
+	uint8_t  padding[16];
+} __attribute__((__packed__)) _app_impt;
+
+_app_impt _app_impt_default = {
+	{ 'I', 'M', 'P', 'T' },
+	0x00000008,
+	0,
+	0,
+	{
+		0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00
+	}
+};
+
+
+
+typedef struct {
+	char     ident[4];
+	uint32_t unknown;
+	uint32_t offset;
+	uint32_t size;
+	uint8_t  padding[16];
+} __attribute__((__packed__)) _app_expt;
+
+_app_expt _app_expt_default = {
+	{ 'E', 'X', 'P', 'T' },
+	0x00000009,
+	0,
+	0,
+	{
+		0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00
+	}
+};
+
+
+
+typedef struct {
+	char     ident[4];
+	uint32_t unknown0;
+	uint32_t offset;
+	uint32_t size;
+	uint32_t unknown1;
+	uint32_t entry;
+	uint32_t origin;
+	uint32_t prog_size;
+} __attribute__((__packed__)) _app_rawd;
+
+_app_rawd _app_rawd_default = {
+	{ 'R', 'A', 'W', 'D' },
+	0x00000001,
+	0,
+	0,
+	0x00000000,
+	0x00000000,
+	0x80A00000,
+	0
+};
+
+
+
+typedef struct {
+	uint32_t str_offset;
+	uint32_t unknown[2];
+	uint32_t offset;
+} __attribute__((__packed__)) _app_impt_entry;
+
+_app_impt_entry _app_impt_entry_default = {
+	0, { 0, 0x00020000 }, 0
+};
+
+
+
+typedef struct {
+	uint32_t str_offset;
+	uint32_t unknown[2];
+	uint32_t offset;
+} __attribute__((__packed__)) _app_expt_entry;
+
+_app_expt_entry _app_expt_entry_default = {
+	0, { 0, 0x00020000 }, 0
+};
+
+
+
+
+uintptr_t _app_strlen(const char* inString) {
+	uintptr_t tempLen = strlen(inString);
+	tempLen += (4 - (tempLen & 3));
+	return tempLen;
+}
+
+app* app_create(void* inData, uintptr_t inSize, uintptr_t inEntry, uintptr_t inBSS) {
+	app* tempApp = (app*)malloc(sizeof(app));
+	if(tempApp == NULL)
+		return NULL;
+
+	tempApp->import_count = 0;
+	tempApp->import_data  = NULL;
+
+	tempApp->export_count = 0;
+	tempApp->export_data  = NULL;
+
+	tempApp->bin_size  = inSize;
+	tempApp->bin_data  = inData;
+	tempApp->bin_entry = inEntry;
+	tempApp->bin_bss   = inBSS;
+
+	return tempApp;
+}
+
+
+void app_delete(app* inApp) {
+	if(inApp == NULL)
+		return;
+
+	uintptr_t i;
+
+	if(inApp->import_data != NULL) {
+		for(i = 0; i < inApp->import_count; i++)
+			free(inApp->import_data[i]);
+		free(inApp->import_data);
+	}
+
+	if(inApp->export_data != NULL) {
+		for(i = 0; i < inApp->export_count; i++)
+			free(inApp->export_data[i]);
+		free(inApp->export_data);
+	}
+
+	if(inApp->bin_data != NULL)
+		free(inApp->bin_data);
+
+	free(inApp);
+}
+
+
+
+bool app_import_add(app* inApp, char* inName, uintptr_t inOffset) {
+	if(inApp == NULL)
+		return false;
+
+	app_import_entry* tempEntry = (app_import_entry*)malloc(sizeof(app_import_entry) + strlen(inName) + 1);
+	if(tempEntry == NULL)
+		return false;
+	tempEntry->offset = inOffset;
+	tempEntry->name = (char*)((uintptr_t)tempEntry + sizeof(app_import_entry));
+	strcpy(tempEntry->name, inName);
+
+	app_import_entry** tempRealloc = (app_import_entry**)realloc(inApp->import_data, (sizeof(app_import_entry*) * (inApp->import_count + 1)));
+	if(tempRealloc == NULL) {
+		free(tempEntry);
+		return false;
+	}
+
+	inApp->import_data = tempRealloc;
+	inApp->import_data[inApp->import_count] = tempEntry;
+	inApp->import_count++;
+
+	return true;
+}
+
+bool app_export_add(app* inApp, char* inName, uintptr_t inOffset) {
+	if(inApp == NULL)
+		return false;
+
+	app_export_entry* tempEntry = (app_export_entry*)malloc(sizeof(app_export_entry) + strlen(inName) + 1);
+	if(tempEntry == NULL)
+		return false;
+	tempEntry->offset = inOffset;
+	tempEntry->name = (char*)((uintptr_t)tempEntry + sizeof(app_export_entry));
+	strcpy(tempEntry->name, inName);
+
+	app_export_entry** tempRealloc = (app_export_entry**)realloc(inApp->export_data, (sizeof(app_export_entry*) * (inApp->export_count + 1)));
+	if(tempRealloc == NULL) {
+		free(tempEntry);
+		return false;
+	}
+
+	inApp->export_data = tempRealloc;
+	inApp->export_data[inApp->export_count] = tempEntry;
+	inApp->export_count++;
+
+	return true;
+}
+
+
+
+void _fprint_string(const char* inString, FILE* inStream) {
+	uintptr_t tempLen = strlen(inString);
+	fwrite(inString, 1, tempLen, inStream);
+	uintptr_t i;
+	for(i = 0; i < (4 - (tempLen & 3)); i++)
+		fwrite(&inString[tempLen], 1, 1, inStream);
+}
+
+bool app_save(app* inApp, const char* inPath) {
+	if((inApp == NULL) || (inPath == NULL))
+		return false;
+
+	uintptr_t i, j;
+
+	_app_ccdl tempCCDL = _app_ccdl_default;
+	_app_impt tempIMPT = _app_impt_default;
+	_app_expt tempEXPT = _app_expt_default;
+	_app_rawd tempRAWD = _app_rawd_default;
+
+	uint32_t tempPadding[4] = { 0, 0, 0, 0 };
+
+	tempIMPT.offset = sizeof(_app_ccdl) + sizeof(_app_impt) + sizeof(_app_expt) + sizeof(_app_rawd);
+
+	tempIMPT.size = (sizeof(_app_impt_entry) * (inApp->import_count + 1));
+	for(i = 0; i < inApp->import_count; i++)
+		tempIMPT.size += _app_strlen(inApp->import_data[i]->name);
+	uintptr_t tempIPad = (16 - (tempIMPT.size & 15)) & 15;
+	tempIMPT.size += tempIPad;
+
+
+	tempEXPT.offset = tempIMPT.offset + tempIMPT.size;
+	tempEXPT.size = (sizeof(_app_expt_entry) * (inApp->export_count + 1));
+	for(i = 0; i < inApp->export_count; i++)
+		tempEXPT.size += _app_strlen(inApp->export_data[i]->name);
+	uintptr_t tempEPad = (16 - (tempEXPT.size & 15)) & 15;
+	tempEXPT.size += tempEPad;
+
+	tempRAWD.offset = tempEXPT.offset + tempEXPT.size;
+	tempRAWD.size   = (inApp->bin_size + 15) & 0xFFFFFFF0;
+	tempRAWD.origin = 0x80A00000;
+	tempRAWD.entry  = inApp->bin_entry;
+	tempRAWD.prog_size = tempRAWD.size + inApp->bin_bss;
+
+	FILE* tempFile = fopen(inPath, "wb");
+	if(tempFile == NULL)
+		return false;
+
+	fwrite(&tempCCDL, sizeof(_app_ccdl), 1, tempFile);
+	fwrite(&tempIMPT, sizeof(_app_impt), 1, tempFile);
+	fwrite(&tempEXPT, sizeof(_app_expt), 1, tempFile);
+	fwrite(&tempRAWD, sizeof(_app_rawd), 1, tempFile);
+	// TODO - Consider extra field.
+
+
+
+	// Write Import Table
+	_app_impt_entry tempIHeader = { inApp->import_count, { 0, 0 }, 0 };
+	fwrite(&tempIHeader, sizeof(_app_impt_entry), 1, tempFile);
+	_app_impt_entry tempIEntry = _app_impt_entry_default;
+	for(i = 0; i < inApp->import_count; i++) {
+		tempIEntry.offset = inApp->import_data[i]->offset;
+		fwrite(&tempIEntry, sizeof(_app_impt_entry), 1, tempFile);
+		tempIEntry.str_offset += _app_strlen(inApp->import_data[i]->name);
+	}
+
+	// Write Import Strings
+	for(i = 0; i < inApp->import_count; i++)
+		_fprint_string(inApp->import_data[i]->name, tempFile);
+
+	// Write Import Whitespace
+	fwrite(tempPadding, 1, tempIPad, tempFile);
+
+
+
+	// Write Export Table
+	_app_expt_entry tempEHeader = { inApp->export_count, { 0, 0 }, 0 };
+	fwrite(&tempEHeader, sizeof(_app_expt_entry), 1, tempFile);
+	_app_expt_entry tempEEntry = _app_expt_entry_default;
+	for(i = 0; i < inApp->export_count; i++) {
+		tempEEntry.offset = inApp->export_data[i]->offset;
+		fwrite(&tempEEntry, sizeof(_app_expt_entry), 1, tempFile);
+		tempEEntry.str_offset += _app_strlen(inApp->export_data[i]->name);
+	}
+
+	// Write Export Strings
+	for(i = 0; i < inApp->export_count; i++)
+		_fprint_string(inApp->export_data[i]->name, tempFile);
+
+	// Write Export Whitespace
+	fwrite(tempPadding, 1, tempEPad, tempFile);
+
+
+
+	// Write Binary Data
+	fwrite(inApp->bin_data, 1, inApp->bin_size, tempFile);
+	fwrite(tempPadding, 1, ((16 - (inApp->bin_size & 15)) & 15), tempFile);
+
+	// TODO - Append res data.
+
+	fclose(tempFile);
+	return true;
+}
---
+++ trunk/dingoo_sdk/tools/elf2app/src/elf.c
@@ -0,0 +1,142 @@
+#include "elf.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+
+
+elf_symbol_table* elf_symbol_table_create() {
+	elf_symbol_table* tempTable = (elf_symbol_table*)malloc(sizeof(elf_symbol_table));
+	if(tempTable == NULL)
+		return NULL;
+	tempTable->size = 0;
+	tempTable->data = NULL;
+	return tempTable;
+}
+
+void elf_symbol_table_delete(elf_symbol_table* inTable) {
+	if(inTable == NULL)
+		return;
+
+	uintptr_t i;
+
+	if(inTable->data != NULL) {
+		for(i = 0; i < inTable->size; i++)
+			free(inTable->data[i]);
+		free(inTable->data);
+	}
+
+	free(inTable);
+}
+
+
+
+bool elf_symbol_table_add(elf_symbol_table* inTable, elf_symbol_entry* inEntry) {
+	if((inTable == NULL) || (inEntry == NULL))
+		return false;
+
+	elf_symbol_entry** tempRealloc = (elf_symbol_entry**)realloc(inTable->data, (sizeof(elf_symbol_entry*) * (inTable->size + 1)));
+	if(tempRealloc == NULL)
+		return false;
+
+	inTable->data = tempRealloc;
+	inTable->data[inTable->size] = inEntry;
+	inTable->size++;
+
+	return true;
+
+}
+
+elf_symbol_entry* elf_symbol_entry_parse(const char* inLine) {
+	uint32_t tempOffset;
+	char     tempName[256];
+
+	tempName[0] = '\0';
+	if(sscanf(inLine, "%lx %*c %*c .dingoo %*x %s", &tempOffset, tempName) < 0)
+		return NULL;
+	if(!isalnum(tempName[0]) && (tempName[0] != '_'))
+		return NULL;
+
+	uintptr_t i;
+	for(i = 1; (isalnum(tempName[i]) || (tempName[i] == '_')); i++);
+	tempName[i] = '\0';
+
+
+	elf_symbol_entry* tempEntry = (elf_symbol_entry*)malloc(sizeof(elf_symbol_entry) + strlen(tempName) + 1);
+	if(tempEntry == NULL)
+		return NULL;
+
+	tempEntry->name = (char*)((uintptr_t)tempEntry + sizeof(elf_symbol_entry));
+	strcpy(tempEntry->name, tempName);
+	tempEntry->offset = tempOffset;
+
+	return tempEntry;
+}
+
+elf_symbol_table* elf_symbol_table_read(const char* inPath) {
+	char tempString[512];
+	sprintf(tempString, "mipsel-linux-objdump --adjust-vma=0x80a00000 -t %s", inPath);
+
+	FILE* tempFile = popen(tempString, "r");
+	if(tempFile == NULL)
+		return NULL;
+
+	elf_symbol_table* tempTable = elf_symbol_table_create();
+	if(tempTable == NULL) {
+		fclose(tempFile);
+		return NULL;
+	}
+
+	while(!feof(tempFile)) {
+		fgets(tempString, 512, tempFile);
+		if(strncmp(tempString, "SYMBOL TABLE:", 13) == 0)
+			break;
+	}
+
+	while(!feof(tempFile)) {
+		fgets(tempString, 512, tempFile);
+		if(tempString[0] == '\n')
+			break;
+		elf_symbol_table_add(tempTable, elf_symbol_entry_parse(tempString));
+	}
+
+	fclose(tempFile);
+	return tempTable;
+}
+
+
+
+uintptr_t elf_symbol_table_find_offset(elf_symbol_table* inTable, const char* inName) {
+	if((inTable == NULL) || (inTable->data == NULL))
+		return 0;
+
+	uintptr_t i;
+	for(i = 0; i < inTable->size; i++) {
+		if(strcmp(inTable->data[i]->name, inName) == 0)
+			return inTable->data[i]->offset;
+	}
+	return 0;
+}
+
+
+
+uintptr_t elf_bss_size(const char* inPath) {
+	char tempString[512];
+	sprintf(tempString, "mipsel-linux-objdump --adjust-vma=0x80a00000 -h %s.elf", inPath);
+	FILE* tempFile = popen(tempString, "rb");
+	if(tempFile == NULL)
+		return 0;
+
+	uintptr_t tempSize;
+	while(!feof(tempFile)) {
+		fgets(tempString, 512, tempFile);
+		if(sscanf(tempString, " %*u %s %lx", tempString, &tempSize) >= 0) {
+			if(strcmp(tempString, ".bss") == 0)
+				return tempSize;
+		}
+	}
+
+	return 0;
+}
