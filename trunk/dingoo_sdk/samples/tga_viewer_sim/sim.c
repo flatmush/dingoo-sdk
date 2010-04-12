c66577fec338b6efa2afcaa9abe1147e874996c1
flatmush <flatmush@52aa303c-2c39-11df-b580-1f2e1e9a714e>
2010-04-12 15:00:39 +0000

    Added a SIM sample, it's a tga viewer that can view any tga files useable or written by sml.

---
+++ trunk/dingoo_sdk/samples/tga_viewer_sim/sim.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+
+/* file extension name */
+int GetFileType(char* pname) {
+	if(pname != NULL)
+		strcpy(pname, "TGA"); // Emulator ROM extensions (use "EXT|EXT|EXT" for several file-type associations)
+	return 0;
+}
+
+/* to get default path */
+int GetDefaultPath(char* path) {
+	if(path != NULL)
+		strcpy(path, "A:\\IMAGE");
+	return 0;
+}
+
+/* module description, optional */
+int GetModuleName(char* name, int code_page) {
+	if((name != NULL) && (code_page == 0)) // ansi
+		strcpy(name, "tga_viewer.sim");  //your emulator file name
+	return 0;
+}
