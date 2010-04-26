ca2349033ef805744c4361f44638491a7a278b7a
jeanmichel.girard <jeanmichel.girard@52aa303c-2c39-11df-b580-1f2e1e9a714e>
2010-04-26 15:33:47 +0000

    add offsetof declaration

--- dingoo_sdk/src/libc/stddef.h
+++ dingoo_sdk/src/libc/stddef.h
@@ -7,6 +7,9 @@
 #define NULL ((void *)0)
 #endif
 
+#define offsetof(st, m) \
+    ((size_t) ( (char *)&((st *)(0))->m - (char *)0 ))
+
 typedef intptr_t ptrdiff_t;
 typedef uintptr_t size_t;
 typedef uint16_t wchar_t;
