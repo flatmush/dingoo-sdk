b7a7facd6da5b385802e75a20669533570fde03b
harteex@gmail.com <harteex@gmail.com@52aa303c-2c39-11df-b580-1f2e1e9a714e>
2010-07-28 16:55:04 +0000

    Revert r154 setjmp changes, makes exit() work again

--- dingoo_sdk/src/libc/setjmp.h
+++ dingoo_sdk/src/libc/setjmp.h
@@ -1,20 +1,11 @@
 #ifndef __libc_setjmp_h__
 #define __libc_setjmp_h__
 
-#ifdef __cplusplus
-extern "C"
-{
-#endif
-
 #include <stdint.h>
 
-typedef uintptr_t jmp_buf[32];
+typedef uintptr_t jmp_buf[31];
 
 extern int  setjmp(jmp_buf env);
 extern void longjmp(jmp_buf env, int value);
-
-#ifdef __cplusplus
-}
-#endif
 
 #endif
