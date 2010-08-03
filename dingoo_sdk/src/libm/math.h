140268d6b2f6d973e5af6da0d94e82f0ef44934e
djdron <djdron@52aa303c-2c39-11df-b580-1f2e1e9a714e>
2010-08-03 22:12:09 +0000

    + libm added (FDLIBM from Sun Microsystems)

---
+++ dingoo_sdk/src/libm/math.h
@@ -0,0 +1,83 @@
+#ifndef __math_h__
+#define __math_h__
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#define M_E			2.7182818284590452354	/* e */
+#define M_LOG2E		1.4426950408889634074	/* log_2 e */
+#define M_LOG10E	0.43429448190325182765	/* log_10 e */
+#define M_LN2		0.69314718055994530942	/* log_e 2 */
+#define M_LN10		2.30258509299404568402	/* log_e 10 */
+#define M_PI		3.14159265358979323846	/* pi */
+#define M_PI_2		1.57079632679489661923	/* pi/2 */
+#define M_PI_4		0.78539816339744830962	/* pi/4 */
+#define M_1_PI		0.31830988618379067154	/* 1/pi */
+#define M_2_PI		0.63661977236758134308	/* 2/pi */
+#define M_2_SQRTPI	1.12837916709551257390	/* 2/sqrt(pi) */
+#define M_SQRT2		1.41421356237309504880	/* sqrt(2) */
+#define M_SQRT1_2	0.70710678118654752440	/* 1/sqrt(2) */
+
+#define MAXFLOAT	((float)3.40282346638528860e+38)
+#define HUGE		MAXFLOAT
+#define X_TLOSS	1.41484755040568800000e+16
+
+extern double acos(double);
+extern double asin(double);
+extern double atan(double);
+extern double atan2(double, double);
+extern double cos(double);
+extern double sin(double);
+extern double tan(double);
+
+extern double cosh(double);
+extern double sinh(double);
+extern double tanh(double);
+
+extern double exp(double);
+extern double frexp(double, int *);
+extern double ldexp(double, int);
+extern double log(double);
+extern double log10(double);
+extern double modf(double, double *);
+
+extern double pow(double, double);
+extern double sqrt(double);
+
+extern double ceil(double);
+extern double fabs(double);
+extern double floor(double);
+extern double fmod(double, double);
+
+extern double erf(double);
+extern double erfc(double);
+extern double gamma(double);
+extern double hypot(double, double);
+extern int isnan(double);
+extern int finite(double);
+extern double j0(double);
+extern double j1(double);
+extern double jn(int, double);
+extern double lgamma(double);
+extern double y0(double);
+extern double y1(double);
+extern double yn(int, double);
+
+extern double acosh(double);
+extern double asinh(double);
+extern double atanh(double);
+extern double cbrt(double);
+extern double logb(double);
+extern double nextafter(double, double);
+extern double remainder(double, double);
+extern double scalb(double, double);
+
+extern int signgam;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif//__math_h__
