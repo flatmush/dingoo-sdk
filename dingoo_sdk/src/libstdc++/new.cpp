0e79a2370803b314af4584d14d5c9c28c96735d3
djdron <djdron@52aa303c-2c39-11df-b580-1f2e1e9a714e>
2010-08-03 22:17:50 +0000

    + libstdc++ prepaired for stlport

--- dingoo_sdk/src/libstdc++/new.cpp
+++ dingoo_sdk/src/libstdc++/new.cpp
@@ -1,7 +1,9 @@
 #include <malloc.h>
+#include <new>
 
 void* operator new(size_t size) { return malloc(size); }
 void* operator new[](size_t size) { return malloc(size); }
 void operator delete(void* p) { free(p); }
 void operator delete[](void* p) { free(p); }
+
 extern "C" void __cxa_pure_virtual() {}
