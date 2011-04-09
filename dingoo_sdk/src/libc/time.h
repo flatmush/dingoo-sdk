5ed7e865fc7d9e1b5dff78e902528a4199144332
djdron <djdron@52aa303c-2c39-11df-b580-1f2e1e9a714e>
2011-04-09 21:18:30 +0000

    + d_type in dirent compatibility improved + localtime stub

--- dingoo_sdk/src/libc/time.h
+++ dingoo_sdk/src/libc/time.h
@@ -12,11 +12,25 @@ extern "C"
 typedef unsigned long int clock_t;
 typedef long int time_t;
 
+struct tm
+{
+  int tm_sec;
+  int tm_min;
+  int tm_hour;
+  int tm_mday;
+  int tm_mon;
+  int tm_year;
+  int tm_wday;
+  int tm_yday;
+  int tm_isdst;
+};
+
 #define CLOCKS_PER_SEC (OS_TICKS_PER_SEC * 10000)
 
 extern clock_t clock();
 
-extern time_t time(time_t* timer);
+extern time_t time(time_t* timer); //stub
+extern struct tm* localtime(const time_t* timer); //stub
 
 #ifdef __cplusplus
 }
